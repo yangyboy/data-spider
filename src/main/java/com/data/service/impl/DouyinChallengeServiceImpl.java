80192fc9b72475669ccfc49e4809bf82b22c51bf
yang <yangyboy@163.com>
2020-07-29 10:03:49 +0800

    爬取话题视频并入库

--- src/main/java/com/data/service/impl/DouyinChallengeServiceImpl.java
+++ src/main/java/com/data/service/impl/DouyinChallengeServiceImpl.java
@@ -3,6 +3,8 @@ package com.data.service.impl;
 import cn.hutool.core.util.StrUtil;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.data.constant.CommonConst;
 import com.data.entity.DouyinChallenge;
@@ -26,6 +28,9 @@ public class DouyinChallengeServiceImpl extends ServiceImpl<DouyinChallengeMappe
     private String challengeVideoUrl;
 
     @Resource
+    private IDouyinChallengeService douyinChallengeService;
+
+    @Resource
     private IDouyinVideoService douyinVideoService;
 
     @Override
@@ -41,6 +46,7 @@ public class DouyinChallengeServiceImpl extends ServiceImpl<DouyinChallengeMappe
             String message = JsoupUtl.getMessage(formatUrl);
             if (StrUtil.isEmpty(message) || !message.startsWith("{")) {
                 log.info("抖音热搜榜查询失败，查询结果为：{}", message);
+                continue;
             }
             JSONObject videoSearchObj = JSON.parseObject(message);
 
@@ -51,12 +57,29 @@ public class DouyinChallengeServiceImpl extends ServiceImpl<DouyinChallengeMappe
                 break;
             }
 
+            /**
+             * 手动put 数据来源，处理数据时入库，方便以后查询数据
+             */
             videoSearchObj.put("source", CommonConst.VideoConstant.VIDEO_SOURCE_CHALLENGE);
             douyinVideoService.handDouyinVideoData(videoSearchObj);
 
             cursor += count;
 
+            /**
+             * 防止请求太快被封ip，每次请求间隔三秒
+             */
+            try {
+                Thread.sleep(2000L);
+            } catch (InterruptedException e) {
+                log.info("爬取话题视频线程被中断",e);
+            }
+
         }
+
+        //证明该话题已爬取完成，修改状态为已爬取
+        douyinChallengeService.update(new LambdaUpdateWrapper<DouyinChallenge>().
+                eq(DouyinChallenge::getCid, cid)
+                .set(DouyinChallenge::getCrawlStatus,CommonConst.ChallengeConstant.CHALLENGE_ALREADY_CRAWL));
 
 
     }
